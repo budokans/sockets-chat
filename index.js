bb1ea32471e8d21f2b9b00aef881f8e94eb20e05
budokans <68995729+budokans@users.noreply.github.com>
2021-07-13 02:32:33 +1200

    Initialize instance of socket.io and listen
    - for connection and disconnection

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get("/", (_, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected!");
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000, () => {
   console.log("Listening on http://localhost:3000");
 });
